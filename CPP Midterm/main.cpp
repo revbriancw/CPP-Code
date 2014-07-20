4a368cadc845c49fc492368413fcbaa1fcc05bf9
Brian Wyatt <brianwyatt@hyperion.local>
2014-07-19 20:21:17 -0400

    Initial commit from local git

---
+++ CPP Midterm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPP Midterm
+//
+//  Created by Brian Wyatt on 19.Jul.2014.
+//  Copyright (c) 2014 BW Development. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
